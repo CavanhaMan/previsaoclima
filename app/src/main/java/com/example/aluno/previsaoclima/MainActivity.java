5c4d090477afb11621fb0883fe254e5ae5fd7385
Bruno Mundim <brunomundim3232@gmail.com>
2018-12-01 16:30:46 -0200

    Criando função de busca dos dados

--- app/src/main/java/com/example/aluno/previsaoclima/MainActivity.java
+++ app/src/main/java/com/example/aluno/previsaoclima/MainActivity.java
@@ -3,11 +3,40 @@ package com.example.aluno.previsaoclima;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.aluno.previsaoclima.model.DadosClima;
+import com.example.aluno.previsaoclima.model.InterfaceDeServicos;
+import com.example.aluno.previsaoclima.service.RetrofitService;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void obtemClima(){
+        InterfaceDeServicos services = RetrofitService.getServico();
+        Call<DadosClima> call = services.consulta(1,1);
+        call.enqueue(new Callback<DadosClima>(){
+
+            @Override
+            public void onResponse(Call<DadosClima> call, Response<DadosClima> response) {
+                
+
+            }
+
+            @Override
+            public void onFailure(Call<DadosClima> call, Throwable t) {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/aluno/previsaoclima/model/DadosClima.java
+++ app/src/main/java/com/example/aluno/previsaoclima/model/DadosClima.java
@@ -4,5 +4,14 @@ public class DadosClima {
     private Double latitude;
     private Double longitude;
 
+    public DadosClima(Double latitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
+    public Double getLatitude() {return latitude;}
+    public void setLatitude(Double latitude) {this.latitude = latitude;}
+
+    public Double getLongitude() {return longitude;}
+    public void setLongitude(Double longitude) {this.longitude = longitude;}
 }
