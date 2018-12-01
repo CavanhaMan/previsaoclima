04e230f6739a59d6f10426eb1e731e3e05a3127a
CavanhaMan <cavanhaman@hotmail.com>
2018-12-01 20:03:08 -0200

    Corrigindo dependencias de mover Interface dos Servicos

--- app/src/main/java/com/example/aluno/previsaoclima/service/RetrofitService.java
+++ app/src/main/java/com/example/aluno/previsaoclima/service/RetrofitService.java
@@ -1,6 +1,6 @@
 package com.example.aluno.previsaoclima.service;
 
-import com.example.aluno.previsaoclima.model.InterfaceDeServicos;
+import com.example.aluno.previsaoclima.service.InterfaceDeServicos;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
