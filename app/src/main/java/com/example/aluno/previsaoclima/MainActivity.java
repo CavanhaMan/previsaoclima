cc7734f711bf349a792f6a29e161d39bcb523470
CavanhaMan <cavanhaman@hotmail.com>
2018-12-01 20:23:06 -0200

    Correcao posicao layout

--- app/src/main/java/com/example/aluno/previsaoclima/MainActivity.java
+++ app/src/main/java/com/example/aluno/previsaoclima/MainActivity.java
@@ -30,54 +30,6 @@ public class MainActivity extends AppCompatActivity {
         lon = (EditText)findViewById(R.id.edlongitude);
 
         button = (Button) findViewById(R.id.btconsulta);
-
-/*        final double latD = Double.parseDouble(lat.getText().toString());
-        final double lonD = Double.parseDouble(lon.getText().toString());
-
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                InterfaceDeServicos services = RetrofitService.getServico();
-
-                Call<DadosClima> call = services.consulta(latD,lonD);
-
-                call.enqueue(new Callback<DadosClima>(){
-                    @Override
-                    public void onResponse(Call<DadosClima> call, Response<DadosClima> response) {
-                        DadosClima dadosClima = response.body();
-                        String tempox=dadosClima.getCurrently().getIcon();
-                        ImageView tempo = (ImageView) findViewById(R.id.imclima);
-
-                        if (tempox.equals("clear-day"))
-                            tempo.setImageResource(R.drawable.clearday);
-                        else if (tempox.equals("clear-night"))
-                            tempo.setImageResource(R.drawable.clearnight);
-                        else if (tempox.equals("cloudy"))
-                            tempo.setImageResource(R.drawable.cloudy);
-                        else if (tempox.equals("fog"))
-                            tempo.setImageResource(R.drawable.fog);
-                        else if (tempox.equals("partly-cloudy-day"))
-                            tempo.setImageResource(R.drawable.partlycloudday);
-                        else if (tempox.equals("partly-cloudy-night"))
-                            tempo.setImageResource(R.drawable.partlycloudynight);
-                        else if (tempox.equals("rain"))
-                            tempo.setImageResource(R.drawable.rain);
-                        else if (tempox.equals("sleet"))
-                            tempo.setImageResource(R.drawable.sleet);
-                        else if (tempox.equals("snow"))
-                            tempo.setImageResource(R.drawable.snow);
-                        else if (tempox.equals("wind"))
-                            tempo.setImageResource(R.drawable.wind);
-                    }
-
-                    @Override
-                    public void onFailure(Call<DadosClima> call, Throwable t) {
-                        Log.i("debug", t.getMessage());
-                    }
-                });
-
-            }
-        });*/
-
     }
 
     public void acaoBotao(View v){
